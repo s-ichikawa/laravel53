dbb316447ef78c3af4622ec8ea6d941b06e47e1e
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-11-16 20:50:58 +0900

    static func

--- sample.php
+++ sample.php
@@ -1,4 +1,16 @@
 <?php
 
-$arr = ['a', 'b'];
-var_dump(key($arr), current($arr));
+function static_func()
+{
+    return static function ($a, $b) {
+        var_dump('func');
+        return $a + $b;
+    };
+}
+
+$test = static_func();
+var_dump($test(1, 2));
+var_dump($test(1,2));
+var_dump($test(3,4));
+//var_dump(static_func(1, 2));
+//var_dump(static_func(3, 4));
